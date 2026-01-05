8af139529f5d9b0cbaa6cae1b8065e7ee6e0f74c
sam-mccarthy <110861054+sam-mccarthy@users.noreply.github.com>
2026-01-05 00:35:09 -0500

    added timing to main file

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use clap::Parser;
 use std::fs::metadata;
 use std::path::{Path, PathBuf};
 use std::{fs, io};
+use std::time::Instant;
 
 #[derive(Parser, Debug)]
 #[command(about, long_about = None)]
@@ -13,21 +14,21 @@ struct Args {
     #[arg(short, long)]
     songs_folder: String,
 
-    /// The starting time of the preview
-    #[arg(long, default_value_t = 20000)]
-    start: u64,
+    /// The starting time of the preview (seconds)
+    #[arg(long, default_value_t = 20.0)]
+    start: f64,
 
-    /// The ending time of the preview
-    #[arg(long, default_value_t = 40000)]
-    end: u64,
+    /// The ending time of the preview (seconds)
+    #[arg(long, default_value_t = 40.0)]
+    end: f64,
 
     /// The duration to fade in the preview
-    #[arg(long, default_value_t = 1000)]
-    fade_in: u64,
+    #[arg(long, default_value_t = 2.0)]
+    fade_in: f64,
 
     /// The duration to fade out the preview
-    #[arg(long, default_value_t = 2000)]
-    fade_out: u64,
+    #[arg(long, default_value_t = 2.0)]
+    fade_out: f64,
 
     /// The filename of the preview file
     #[arg(long, default_value = "preview_auto_generated.ogg")]
@@ -35,6 +36,9 @@ struct Args {
 
     #[arg(long, default_value_t = false)]
     mono_audio: bool,
+    
+    #[arg(long, default_value_t = 48000)]
+    sample_rate: u32
 }
 
 fn get_bms_files(files: &mut Vec<PathBuf>, dir: &Path) -> io::Result<()> {
@@ -66,25 +70,31 @@ fn get_bms_files(files: &mut Vec<PathBuf>, dir: &Path) -> io::Result<()> {
 
 fn main() {
     let args = Args::parse();
-
-    if !metadata(&args.songs_folder)
-        .expect("songs folder missing")
-        .is_dir()
-    {
+    
+    let song_folder = Path::new(&args.songs_folder);
+    if !song_folder.exists() || !song_folder.is_dir() {
+        println!("bad songs folder");
         return;
     }
 
-    let song_folder = Path::new(&args.songs_folder);
     let mut bms_files = Vec::new();
     get_bms_files(&mut bms_files, song_folder).expect("failed to get BMS files");
 
     for file in bms_files {
+        print!("processing {}", file.to_str().unwrap());
+        let start = Instant::now();
+        
         let Ok(render) = Renderer::new(file) else {
             continue;
         };
+        
         if let Err(e) = render.process_bms_file(&args) {
             eprintln!("{}", e);
         }
+        
+        let end = Instant::now();
+        println!(" - took {:.2}s", (end - start).as_secs_f64());
+        
         return;
     }
 }
